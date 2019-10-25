26f3d7329fa5257836653a1124f9aaf720e36705
Dieter <vanderdt@ucmail.uc.edu>
2019-10-25 13:45:24 -0400

    Added possible sign inversion

--- Run.py
+++ Run.py
@@ -1,20 +1,13 @@
 import MyPhidgets
 from matplotlib import pyplot
 
-duration = 60 * 5
+duration = 60 * 3
 wait_scale = 0
-set_points = [40, 40, 40, 40]
-t = MyPhidgets.ThermoTiles(set_points=set_points, do_plot=True)
+set_points = [20, 40, 40, 20]
+control_signs = [-1, 1, 1, -1]
+t = MyPhidgets.ThermoTiles(set_points=set_points, control_signs=control_signs, do_plot=True)
 t.run(duration=duration, wait_scale=wait_scale)
 
-pyplot.figure()
-pyplot.subplot(2,2,1)
-pyplot.hist(t.log.t0)
-pyplot.subplot(2,2,2)
-pyplot.hist(t.log.t1)
-pyplot.subplot(2,2,3)
-pyplot.hist(t.log.t2)
-pyplot.subplot(2,2,4)
-pyplot.hist(t.log.t3)
-
-pyplot.show()
+df = t.logger.export()
+df['error0'] = df['t0'] - df['sp0']
+pyplot.plot(df.error0)

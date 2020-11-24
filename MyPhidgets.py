c13c5d4f49a86fd043555373292bc002834d8bae
Dieter <vanderdt@ucmail.uc.edu>
2020-11-24 12:08:07 -0500

    before making the PID output binary

--- MyPhidgets.py
+++ MyPhidgets.py
@@ -4,48 +4,64 @@ import simple_pid
 from Phidget22.Devices.DigitalOutput import *
 from Phidget22.Devices.TemperatureSensor import *
 from Phidget22.PhidgetException import *
+from Phidget22.LogLevel import *
+from Phidget22.Devices.Log import *
 import Display
 import traceback
 import math
 
-hub_serial = 560175
+Log.enable(LogLevel.PHIDGET_LOG_INFO, "phidget_log.log")
+
+# Serial numbers of the hub(s) and port on those hubs
+relay_hub = [561064, 0]
+thermal_hub = [560175, 0]
 
 
-class MyTile:
+class MyRelay:
     def __init__(self, hub_serial, hub_port, relay_channel):
+        self.updated_n = 0
         self.relay = DigitalOutput()
         self.relay.setChannel(relay_channel)
         self.relay.setHubPort(hub_port)
         self.relay.setDeviceSerialNumber(hub_serial)
-        self.relay.openWaitForAttachment(5000)
         self.relay_channel = relay_channel
+        self.relay.openWaitForAttachment(5000)
         self.previous_duty_cycle = 100
 
     def set_state(self, state):
         self.relay.setState(state)
 
-    def set_duty_cyclce(self, duty_cycle):
+    def reconnect(self):
+        self.relay.close()
+        self.relay.openWaitForAttachment(5000)
+
+    def set_duty_cycle(self, duty_cycle):
+        if self.updated_n%50==0: self.reconnect()
+
         try:
             diff = math.fabs(self.previous_duty_cycle - duty_cycle)
             if diff > 0.05:
                 self.relay.setDutyCycle(duty_cycle)
                 self.previous_duty_cycle = duty_cycle
+                self.updated_n = self.updated_n + 1
         except PhidgetException as exception:
-            print('Error on tile channel', self.relay_channel)
+            print('Error on relay channel', self.relay_channel, exception)
+            self.reconnect()
 
     def __del__(self):
         self.relay.close()
 
 
+
 class MyThermo:
-    def __init__(self, hub_serial, hub_port, thermo_channel):
+    def __init__(self, hub_serial, hub_port, thermal_channel):
         self.sensor = TemperatureSensor()
         self.sensor.setHubPort(hub_port)
-        self.sensor.setChannel(thermo_channel)
+        self.sensor.setChannel(thermal_channel)
         self.sensor.setDeviceSerialNumber(hub_serial)
         self.sensor.openWaitForAttachment(5000)
         self.previous_temp = 0
-        self.thermo_channel = thermo_channel
+        self.thermal_channel = thermal_channel
 
     def get_temp(self):
         try:
@@ -53,24 +69,18 @@ class MyThermo:
             self.previous_temp = temp
             return temp
         except PhidgetException as exception:
-            print('Error on thermo channel', self.thermo_channel)
+            print('Error on thermal channel', self.thermal_channel, exception)
             return self.previous_temp
 
     def __del__(self):
         self.sensor.close()
 
 
-class ThermoTile_Single:
-    def __init__(self, set_point, hub_serial, channels, control_sign=1):
-        # Assume that relay channel 0 (tile) is instrumented with thermocouple 0, etc
-
-        # The phidget solid state relay should be connected to port 0 on the serial hub
-        # The thermocouple  phidget should be connected to port 1 on the serial hub
-        hub_port_tile = 0
-        hub_port_thermo = 1
+class SingleThermalTile:
+    def __init__(self, set_point, channel, control_sign=1):
         self.control_sign = control_sign
-        self.tile = MyTile(hub_serial, hub_port_tile, channels)
-        self.thermo = MyThermo(hub_serial, hub_port_thermo, channels)
+        self.relay = MyRelay(relay_hub[0], relay_hub[1], channel)
+        self.thermal = MyThermo(thermal_hub[0], thermal_hub[1], channel)
         self.pid = simple_pid.PID()
 
         self.pid.setpoint = set_point
@@ -79,16 +89,17 @@ class ThermoTile_Single:
         self.duty_cycle = 0.5
 
     def step(self):
-        current = self.thermo.get_temp()
+        current = self.thermal.get_temp()
         output = self.pid(current)
-        self.duty_cycle = self.duty_cycle + output * self.control_sign
+        self.duty_cycle = self.duty_cycle + (output * self.control_sign)
         if self.duty_cycle < 0: self.duty_cycle = 0
         if self.duty_cycle > 1: self.duty_cycle = 1
-        self.tile.set_duty_cyclce(self.duty_cycle)
+        #self.relay.set_duty_cycle(self.duty_cycle)
+        self.relay.set_state(self.duty_cycle)
         return current
 
 
-class ThermoTiles:
+class ThermalTiles:
     def __init__(self, set_points, control_signs=(1, 1, 1, 1), log_name='log.xls', do_plot=True):
         self.log_name = log_name
         self.do_plot = do_plot
@@ -98,14 +109,10 @@ class ThermoTiles:
         self.save_interval = 25
         self.display = False
 
-        self.thermo_tile_0 = ThermoTile_Single(set_point=set_points[0], control_sign=control_signs[0],
-                                               hub_serial=hub_serial, channels=0)
-        self.thermo_tile_1 = ThermoTile_Single(set_point=set_points[1], control_sign=control_signs[1],
-                                               hub_serial=hub_serial, channels=1)
-        self.thermo_tile_2 = ThermoTile_Single(set_point=set_points[2], control_sign=control_signs[2],
-                                               hub_serial=hub_serial, channels=2)
-        self.thermo_tile_3 = ThermoTile_Single(set_point=set_points[3], control_sign=control_signs[3],
-                                               hub_serial=hub_serial, channels=3)
+        self.thermal_tile_0 = SingleThermalTile(set_point=set_points[0], channel=0, control_sign=control_signs[0])
+        self.thermal_tile_1 = SingleThermalTile(set_point=set_points[1], channel=1, control_sign=control_signs[1])
+        self.thermal_tile_2 = SingleThermalTile(set_point=set_points[2], channel=2, control_sign=control_signs[2])
+        self.thermal_tile_3 = SingleThermalTile(set_point=set_points[3], channel=3, control_sign=control_signs[3])
         if self.do_plot: self.display = Display.Display(set_points)
 
     def print(self, iteration, time_running, t0, t1, t2, t3):
@@ -118,28 +125,25 @@ class ThermoTiles:
         t = '%05i' % time_running
         temps = '%02.2f %02.2f %02.2f %02.2f' % (t0, t1, t2, t3)
         errors = '%+06.2f %+06.2f %+06.2f %+06.2f' % (error_0, error_1, error_2, error_3)
-
         print(i, t, '|', temps, '|', errors)
 
-    def run(self, duration=None, wait_scale=1):
-        between_polling_sleep = 0.01 * wait_scale
-        iteration_sleep = 0.01 * wait_scale
+    def run(self, duration=None, update_sleep=1, iteration_sleep=1):
         iteration = 0
         start_time = time.time()
         while True:
             stamp = time.asctime()
             # Update channel 0
-            t0 = self.thermo_tile_0.step()
-            time.sleep(between_polling_sleep)
+            t0 = self.thermal_tile_0.step()
+            time.sleep(update_sleep)
             # Update channel 1
-            t1 = self.thermo_tile_1.step()
-            time.sleep(between_polling_sleep)
+            t1 = self.thermal_tile_1.step()
+            time.sleep(update_sleep)
             # Update channel 2
-            t2 = self.thermo_tile_2.step()
-            time.sleep(between_polling_sleep)
+            t2 = self.thermal_tile_2.step()
+            time.sleep(update_sleep)
             # Update channel 3
-            t3 = self.thermo_tile_3.step()
-            time.sleep(between_polling_sleep)
+            t3 = self.thermal_tile_3.step()
+            time.sleep(update_sleep)
 
             time_running = time.time() - start_time
             # Print output
@@ -165,8 +169,8 @@ class ThermoTiles:
             time.sleep(iteration_sleep)
             iteration = iteration + 1
 
-        self.thermo_tile_0.tile.set_duty_cyclce(0)
-        self.thermo_tile_1.tile.set_duty_cyclce(0)
-        self.thermo_tile_2.tile.set_duty_cyclce(0)
-        self.thermo_tile_3.tile.set_duty_cyclce(0)
+        self.thermal_tile_0.relay.set_duty_cycle(0)
+        self.thermal_tile_1.relay.set_duty_cycle(0)
+        self.thermal_tile_2.relay.set_duty_cycle(0)
+        self.thermal_tile_3.relay.set_duty_cycle(0)
         print('Done')
--- Run.py
+++ Run.py
@@ -2,11 +2,10 @@ import MyPhidgets
 from matplotlib import pyplot
 
 duration = 60 * 12
-wait_scale = 1
 set_points = [20, 30, 20, 30]
 control_signs = [-1, 1, -1, 1]
-t = MyPhidgets.ThermoTiles(set_points=set_points, control_signs=control_signs, do_plot=False)
-t.run(duration=duration, wait_scale=wait_scale)
+t = MyPhidgets.ThermalTiles(set_points=set_points, control_signs=control_signs, do_plot=False)
+t.run(duration=duration, iteration_sleep=0.01, update_sleep=0.01)
 
 df = t.logger.export()
 df['error0'] = df['t0'] - df['sp0']
